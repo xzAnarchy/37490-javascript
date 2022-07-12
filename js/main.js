306878743b1499babb1adc250fa234c5faa56d0d
zAnarchy <mendozagabriel726@gmail.com>
2022-07-11 20:03:32 -0400

    cambios-JS-eliminacion-img

--- js/main.js
+++ js/main.js
@@ -1,67 +1,111 @@
 function saludo(nombre, apellido) {
-    alert(`Hola ${nombre} ${apellido}.  Bienvenido a LoremShop!`);
-} // Fin de la función saludo 
 
+    alert(`Hola ${nombre}${apellido}.  Bienvenido a LoremShop!`);
 
-//Se ejecuta la funcion saludo
+}
+
 const nombre = prompt("Ingrese su nombre: ");
+
 const apellido = prompt("Ingrese su apellido: ");
+
 saludo(nombre, apellido);
 
+class Producto {
 
-class producto {
     constructor(nombre, precio) {
+
         this.nombre = nombre;
+
         this.precio = precio;
+
     }
+
 }
 
 let listaProductos = [
+
     {nombre: 'Producto beta 1', precio: 10},
+
     {nombre: 'Producto beta 2', precio: 20},
+
     {nombre: 'Producto beta 3', precio: 30},
+
     {nombre: 'Producto beta 4', precio: 40},
+
     {nombre: 'Producto beta 5', precio: 50},
+
     {nombre: 'Producto beta 6', precio: 60},
+
     {nombre: 'Producto beta 7', precio: 70},
-    {nombre: 'Producto beta 8', precio: 80},
+
+    {nombre: 'mouse', precio: 80},
+
     {nombre: 'Producto beta 9', precio: 90},
+
     {nombre: 'Producto beta 10', precio: 100},
+
 ];
 
 const agregarProducto = () => {
+
     confirmar = confirm("¿Desea agregar un nuevo producto?");
+
     if (confirmar) {
+
         let nombre = prompt("Ingrese el nombre del producto: ");
-        let precio = prompt("Ingrese el precio del producto: ");
-        listaProductos.push({ nombre: nombre, precio: precio });
+
+        let precio = parseFloat(prompt("Ingrese el precio del producto: "));
+
+        listaProductos.push(new Producto(nombre,precio));
+
         alert("Producto agregado exitosamente!");
+
     }
+
 }
 
 agregarProducto();
 
+let confirmarEliminar = confirm("¿Desea eliminar un producto?");
 
-confirm = confirm("¿Desea eliminar un producto?");
-if (confirm) {
+if (confirmarEliminar) {
+
     let nombre = prompt("Ingrese el nombre del producto: ");
-    let precio = prompt("Ingrese el precio del producto: ");
-    listaProductos.pop({ nombre: nombre, precio: precio });
-    alert("Producto eliminado exitosamente!");
-}
 
-const mostrarProductos = () => {
-    confirm = confirm("¿Desea ver los productos disponibles?");
-    if (confirm) {
-        let lista = "";
-        for (let i = 0; i < listaProductos.length; i++) {
-            lista += `${listaProductos[i].nombre} - ${listaProductos[i].precio} \n`;
-        }
-        alert(lista);
+    //Verifico si existe (recibo un booleano)
+
+    let existe = listaProductos.some(elemento => elemento.nombre === nombre)
+
+    //Guardo en un nuevo array, los nombres de los productos
+
+    const buscarIndice = listaProductos.map(elemento => elemento.nombre)
+
+    //Busco por el índice, del array buscarIndice
+
+    let indice = buscarIndice.indexOf(nombre)
+
+    if(existe){
+
+        listaProductos.splice(indice,1)
+
+        alert("Producto eliminado exitosamente!");
+
+    }else{
+
+        alert("Producto no encontrado")
+
     }
+
 }
 
-mostrarProductos();
+let verProductos = confirm("¿Desea ver los productos disponibles?");
 
+if (verProductos) {
 
+    for (let i = 0; i < listaProductos.length; i++) {
 
+        alert(`${listaProductos[i].nombre} - ${listaProductos[i].precio}`);
+
+    }
+
+}
