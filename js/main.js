ab66ac0e487c1e6188295129dd98861cce23191e
zAnarchy <mendozagabriel726@gmail.com>
2022-07-19 09:57:29 -0400

    agregar-sistema-carrito

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/*
 function saludo(nombre, apellido) {
 
     alert(`Hola ${nombre}${apellido}.  Bienvenido a LoremShop!`);
@@ -32,17 +33,7 @@ let listaProductos = [
 
     {nombre: 'Producto beta 4', precio: 40},
 
-    {nombre: 'Producto beta 5', precio: 50},
-
-    {nombre: 'Producto beta 6', precio: 60},
-
-    {nombre: 'Producto beta 7', precio: 70},
-
-    {nombre: 'mouse', precio: 80},
-
-    {nombre: 'Producto beta 9', precio: 90},
-
-    {nombre: 'Producto beta 10', precio: 100},
+    {nombre: 'mouse', precio: 50},
 
 ];
 
@@ -109,3 +100,85 @@ if (verProductos) {
     }
 
 }
+*/
+
+//Inicio DOM
+
+// como abrir y cerrar el carrito
+const carrito = document.querySelector("#cart");
+const cartModalOverlay = document.querySelector(".cart-modal-overlay"); 
+
+//abrir
+carrito.addEventListener("click", ()=>{
+    if(cartModalOverlay.classList.contains("open")) {
+        cartModalOverlay.classList.remove("open");
+    } else {
+        cartModalOverlay.classList.add("open");
+    }
+})
+
+//cerrar
+const closeBtn = document.querySelector("#close-btn");
+
+closeBtn.addEventListener("click", ()=>{
+    cartModalOverlay.classList.remove("open");
+})
+
+//agregar elementos al carrito
+const addToCart = document.getElementsByClassName("add-to-cart");
+console.log(addToCart)
+for(let boton of addToCart) {
+    boton.addEventListener("click", compilarDatos)
+}
+function compilarDatos(e){
+    let boton = e.target;
+    let producto = boton.parentElement;
+    let prodID = producto.getAttribute("id");
+    let prodName = producto.querySelector("h3").innerText;
+    let precio = producto.querySelector(".product-price").innerText;
+    let imagen = producto.querySelector(".product-image").src;
+    agregarElemento(prodID,prodName,precio,imagen)
+}
+
+function agregarElemento(prodID,prodName,precio,imagen) {
+    let productRow = document.createElement("div");
+    let contenedorProductos = document.querySelector(".product-rows");
+
+    let elemProducto = `
+        <div class="product-row" id="${prodID}">
+            <img class="cart-image" src="${imagen}" />
+            <h2>${prodName}:</h2>
+            <span class="cart-price"> ${precio}</span>
+            <button class="remove-btn">Borrar</button>
+        </div>
+    `
+    productRow.innerHTML = elemProducto;
+    contenedorProductos.append(productRow);
+    let botonesBorrar = productRow.querySelectorAll(".remove-btn");
+    for(let boton of botonesBorrar) {
+        boton.addEventListener("click", borrarElemento);
+    }
+    cantElementosCarrito();
+}
+function borrarElemento(e) {
+    btn = e.target;
+    btn.parentElement.parentElement.remove();
+    cantElementosCarrito()
+}
+
+function cantElementosCarrito() {
+    let cantidad = document.querySelectorAll(".product-rows > div");
+    let cartQuantity = document.querySelector(".cart-quantity");
+    cartQuantity.innerText = cantidad.length;
+
+}
+
+//Suma precios carrito
+const sumaPrecios = document.querySelector(".total-price");
+const productRows = document.querySelectorAll(".product-row");
+let total = 0;
+for(let producto of productRows) {
+    let precio = producto.querySelector(".cart-price").innerText;
+    total += parseFloat(precio);
+}
+sumaPrecios.innerText = total;
