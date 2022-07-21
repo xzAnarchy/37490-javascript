715c6d2c402523655d1676e81b043a21cdfffda5
zAnarchy <mendozagabriel726@gmail.com>
2022-07-21 18:16:22 -0400

    limpiar codigo comentado

---
+++ js/register.js
@@ -0,0 +1,33 @@
+// Variables
+let boton = document.querySelector("#boton-register");
+let usuario = document.getElementById("usuario");
+let clave = document.getElementById("clave");
+let afterRegister = document.querySelector("#login-register");
+
+
+//Mensaje de error
+let mjsError = document.createElement("div");
+mjsError.classList.add("error-form-register");
+
+//Mensajes de success
+let mjssucess = document.createElement("div");
+mjssucess.classList.add("success-form-register");
+
+boton.onclick =  (e) =>{
+    e.preventDefault();
+    if(usuario.value.length == 0 || clave.value.length == 0) {
+        mjsError.innerHTML = `<p>No puedes dejar campos vacios</p>`;
+        document.querySelector("#formulario").append(mjsError);
+    }else{
+        e.preventDefault();
+        document.getElementById("formulario").submit();
+        localStorage.setItem("usuario", usuario.value);
+        localStorage.setItem("clave", clave.value);
+        mjssucess.innerHTML = `<p>Usuario registrado correctamente</p>`;
+        document.querySelector("#formulario").append(mjssucess);
+    }
+}
+
+if (localStorage.getItem("usuario") != null) {
+    afterRegister.innerHTML = `<p class="usuario-nav">Bienvenido ${localStorage.getItem("usuario")}</p>`;
+}
