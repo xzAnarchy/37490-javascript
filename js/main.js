0a3cde2aa8118268a42d01f756de983279af8daf
zAnarchy <mendozagabriel726@gmail.com>
2022-07-28 18:06:41 -0400

    ordene el js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-
+//Variables globales
 class Producto {
     constructor(nombre, precio) {
         this.nombre = nombre;
-        this.precio = precio;
+        this.precio = parseFloat(precio);
     }
 }
 
@@ -20,7 +20,46 @@ let listaProductos = [
 
 ];
 
-//Mostrar productos
+let totalPrecio= 0;
+let total = document.querySelector(".total-price");
+let afterRegister = document.querySelector("#login-register");
+const carrito = document.querySelector("#cart");
+const cartModalOverlay = document.querySelector(".cart-modal-overlay"); 
+const closeBtn = document.querySelector("#close-btn");
+const shoppingCart = [];
+const addToCart = document.getElementsByClassName("add-to-cart");
+
+mostrarProductos();
+
+//Eventos
+
+//abrir dando click en el icono del carrito
+carrito.addEventListener("click", ()=>{
+    if(cartModalOverlay.classList.contains("open")) {
+        cartModalOverlay.classList.remove("open");
+    } else {
+        cartModalOverlay.classList.add("open");
+    }
+})
+
+//cerrar modal dando click en el boton de cerrar
+closeBtn.addEventListener("click", ()=>{
+    cartModalOverlay.classList.remove("open");
+})
+
+//agregar elementos al carrito
+for(let boton of addToCart) {
+    boton.addEventListener("click", compilarDatos)
+}
+
+//Cambiar login y register
+if (localStorage.getItem("usuario") != null) {
+    afterRegister.innerHTML = `<p class="usuario-nav">Bienvenido ${localStorage.getItem("usuario")}</p>`;
+}
+
+
+//========================================Funciones=========================================================== 
+//Muestra los productos en el Html
 function mostrarProductos() {
     let lista = document.getElementById('lista-productos');
     let productos = listaProductos;
@@ -30,7 +69,7 @@ function mostrarProductos() {
             <div class="product-row card-main card-${i}" id="${i}">
                 <img class="product-image" src="assets/producto${i+1}.jpg" />
                 <h3>${producto.nombre}</h3>
-                <span class="product-price">$${producto.precio}</span>
+                <p>$<span class="product-price">${producto.precio}</span></p>
                 <p>Lorem ipsum dolor sit amet consectetur.</p>
                 <button class="add-to-cart">Comprar</button>
             </div>
@@ -39,37 +78,7 @@ function mostrarProductos() {
     }
 }
 
-
-mostrarProductos();
-
-//Inicio DOM Carrito
-
-// como abrir y cerrar el carrito
-const carrito = document.querySelector("#cart");
-const cartModalOverlay = document.querySelector(".cart-modal-overlay"); 
-
-//abrir
-carrito.addEventListener("click", ()=>{
-    if(cartModalOverlay.classList.contains("open")) {
-        cartModalOverlay.classList.remove("open");
-    } else {
-        cartModalOverlay.classList.add("open");
-    }
-})
-
-//cerrar
-const closeBtn = document.querySelector("#close-btn");
-
-closeBtn.addEventListener("click", ()=>{
-    cartModalOverlay.classList.remove("open");
-})
-
-//agregar elementos al carrito
-const shoppingCart = [];
-const addToCart = document.getElementsByClassName("add-to-cart");
-for(let boton of addToCart) {
-    boton.addEventListener("click", compilarDatos)
-}
+//Agrega los elementos al carrito cuando se le da al boton de agregar
 function compilarDatos(e){
     let boton = e.target;
     let producto = boton.parentElement;
@@ -79,9 +88,11 @@ function compilarDatos(e){
     let imagen = producto.querySelector(".product-image").src;
     agregarElemento(prodID,prodName,precio,imagen)
     shoppingCart.push(new Producto(prodName,precio));
+    console.log(shoppingCart)
     sumarPrecios();
 }
 
+//crear elementos en el carrito
 function agregarElemento(prodID,prodName,precio,imagen) {
     let productRow = document.createElement("div");
     let contenedorProductos = document.querySelector(".product-rows");
@@ -90,7 +101,7 @@ function agregarElemento(prodID,prodName,precio,imagen) {
         <div class="product-row" id="${prodID}">
             <img class="cart-image" src="${imagen}" />
             <h2>${prodName}:</h2>
-            <span class="cart-price"> ${precio}</span>
+            <p>$<span class="cart-price"> ${precio}</span></p>
             <button class="remove-btn">Borrar</button>
         </div>
     `
@@ -102,12 +113,15 @@ function agregarElemento(prodID,prodName,precio,imagen) {
     }
     cantElementosCarrito();
 }
+
+//borrar elementos del carrito
 function borrarElemento(e) {
     btn = e.target;
     btn.parentElement.parentElement.remove();
     cantElementosCarrito()
 }
 
+//Muestra la cantidad de elementos en el carrito
 function cantElementosCarrito() {
     let cantidad = document.querySelectorAll(".product-rows > div");
     let cartQuantity = document.querySelector(".cart-quantity");
@@ -115,19 +129,23 @@ function cantElementosCarrito() {
 
 }
 
-//sumar los precios
-let total = document.querySelector(".total-price");
+//sumar precios en el carrito
 function sumarPrecios() {
-    total.innerText = shoppingCart.reduce((total, producto) => {
-        return total + parseInt(producto.precio);
-    }
-    , 0);
+    totalPrecio = shoppingCart.reduce((acumulador, producto) => acumulador + parseInt(producto.precio), 0);
+    total.innerText = `$${totalPrecio}`
 }
 
-
-
-//Cambiar login y register
-let afterRegister = document.querySelector("#login-register");
-if (localStorage.getItem("usuario") != null) {
-    afterRegister.innerHTML = `<p class="usuario-nav">Bienvenido ${localStorage.getItem("usuario")}</p>`;
+//actualizar precio al eliminar un elemento
+function actualizarPrecio(e) {
+    let boton = e.target;
+    let producto = boton.parentElement;
+    let prodID = producto.getAttribute("id");
+    let prodName = producto.querySelector("h3").innerText;
+    let precio = producto.querySelector(".product-price").innerText;
+    let imagen = producto.querySelector(".product-image").src;
+    eliminarElemento(prodID,prodName,precio,imagen)
+    if (shoppingCart.find(producto => producto.nombre === prodName)) {
+        shoppingCart.splice(shoppingCart.indexOf(shoppingCart.find(producto => producto.nombre === prodName)), 1);
+        console.log(shoppingCart)
+    }
 }
